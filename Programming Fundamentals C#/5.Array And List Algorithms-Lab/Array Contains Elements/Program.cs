0d48e548c0d441f968ca8947acf6e0afc12aa53e
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 21:03:19 +0300

    Upload of new homework exercices

--- Programming Fundamentals C#/5.Array And List Algorithms-Lab/Array Contains Elements/Program.cs
+++ Programming Fundamentals C#/5.Array And List Algorithms-Lab/Array Contains Elements/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Read a list of integers on the first line of the console and an integer N from the second line of the console and print whether the
+element is contained in the array.If it is, print “yes”, otherwise print “no”.*/
+
 namespace ArrayContainsElements
 {
     class Program
