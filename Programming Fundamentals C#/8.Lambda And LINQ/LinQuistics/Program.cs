af9e19d76d4654af2552a7a7d44fd4fe85b6a847
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 20:26:47 +0300

    Upload of new homework exercices

--- Programming Fundamentals C#/8.Lambda And LINQ/LinQuistics/Program.cs
+++ Programming Fundamentals C#/8.Lambda And LINQ/LinQuistics/Program.cs
@@ -4,6 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* LINQ is the greatest .NET component of all time. You can do almost anything with it. That’s why you have been tasked to do almost everything you can with it.
+You will be given several input lines containing information about collections, and LINQ methods that have been called on them, in the following format:
+{collection}.{method1}().{method2}(). . .
+The count of methods, may vary. Your task is to store every collection and the methods that have been executed on them. If the collection already exists, you must ADD the new methods to it. Duplicate methods should be REMOVED.
+If you are given a collection name ONLY, you must print the methods that have been executed on the collection, in descending order by their length. If 2 methods have the same length, order them in descending order, by the count of unique symbols they have in their names. The methods must be printed – each on a new line, with an asterisk (“*”) and a space (“ “) prefix.
+If the collection name does NOT exist, you should IGNORE that line of input. 
+If you are given a digit ONLY, you must take the collection, with the most methods, and print the first N methods, with the LOWEST length (N being the digit given from the input). If there are less than N methods you must print all of them, in the same order.
+NOTE: When printing, you must print only the method name, without its brackets.
+The input sequence ends when you receive the command “exit”. After the ending command, you will receive one last line in the following format:
+{method} {selection}
+You must take all collections, which CONTAIN the given method, and print them. The selection will either be “collection” or “all”. 
+If you have “collection”, you must print only the collections’ names in the final output.
+If you have “all”, you must print the collections and their methods in the following format:
+“{collection}
+ * {method1}
+ * {method2}
+ . . .”
+The collections, must be printed in descending order, by the count of methods. If 2 collections have the same count, print the one with the higher MINIMAL method length, first. The methods must be printed in descending order by their length.*/
+
 namespace LinQuistics
 {
     class Program
