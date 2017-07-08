cb4d69b27d2477c70c25ed64f104da63bf8a93b8
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 22:25:58 +0300

    Update Program.cs

--- Programming Fundamentals C#/4.Lists-Exercices/Integer Insertion/Program.cs
+++ Programming Fundamentals C#/4.Lists-Exercices/Integer Insertion/Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* You will receive a list of integers on the same line (separated by one space). On the next lines, you will start receiving a list of strings, until you receive the string “end”. Your task is to insert each string (converted to integer) at a specific index in the list. The index is determined by the first digit of the number.
+Example: 514 -> first digit – 5 -> insert 514 at the 5th index of the list.
+After you insert all the elements, print the list, separated by single spaces.
+The input will always be valid and you don’t need to explicitly check if you’re inserting an element into a valid index.*/
+
 namespace IntegerInsertion
 {
     class Program
@@ -23,10 +28,6 @@ namespace IntegerInsertion
                 var firstDigit = digits[0];
 
                 numbers.Insert(currentNumber[firstDigit]);
-
-
-
-
                 input = Console.ReadLine();
             }
         }
