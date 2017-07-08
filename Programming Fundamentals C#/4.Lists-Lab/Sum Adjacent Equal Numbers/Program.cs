7166d72f4b7078028be61d706e0a4fe67dd2d98a
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 22:09:33 +0300

    Upload of new homework exercices

--- Programming Fundamentals C#/4.Lists-Lab/Sum Adjacent Equal Numbers/Program.cs
+++ Programming Fundamentals C#/4.Lists-Lab/Sum Adjacent Equal Numbers/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Write a program to sum all adjacent equal numbers in a list of decimal numbers, starting from left to right.
+	After two numbers are summed, the obtained result could be equal to some of its neighbors and should be summed as well 
+	Always sum the leftmost two equal neighbors (if several couples of equal neighbors are available).*/
+
 namespace SumAdjacentEqualNumbers
 {
     class Program
