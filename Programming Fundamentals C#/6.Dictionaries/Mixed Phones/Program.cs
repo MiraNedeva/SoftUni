315d9a651326405744f9af7d7f021c4815342441
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 20:46:33 +0300

    Upload of new homework exercices

--- Programming Fundamentals C#/6.Dictionaries/Mixed Phones/Program.cs
+++ Programming Fundamentals C#/6.Dictionaries/Mixed Phones/Program.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* You will be given several phone entries, in the form of strings in format:
+{firstElement} : {secondElement}
+The first element is usually the person’s name, and the second one – his phone number. 
+The phone number consists ONLY of digits, while the person’s name can consist of any ASCII characters. 
+Sometimes the phone operator gets distracted by the Minesweeper she plays all day, and gives you first the phone, and then the name. e.g. : 0888888888 : Pesho. 
+You must store them correctly, even in those cases.
+When you receive the command “Over”, you are to print all names you’ve stored with their phones. 
+The names must be printed in alphabetical order.*/
+
 namespace MixedPhones
 {
     class Program
