e05455a0fa09e45045b0b25be2c4203a506e22a1
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 22:07:09 +0300

    Upload of new  homework exercices

--- Programming Fundamentals C#/4.Lists-Lab/Remove Negatives And Reverse/Program.cs
+++ Programming Fundamentals C#/4.Lists-Lab/Remove Negatives And Reverse/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Read a list of integers, remove all negative numbers from it and print the remaining elements in reversed order. 
+In case of no elements left in the list, print “empty”.*/
+
 namespace RemoveNegativesAndReverse
 {
     class Program
