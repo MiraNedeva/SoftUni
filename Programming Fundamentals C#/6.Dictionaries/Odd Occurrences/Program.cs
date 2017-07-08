0c73c862a1c85401c7191399ac7c2ef9bcde9448
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 20:42:33 +0300

    Update Program.cs

--- Programming Fundamentals C#/6.Dictionaries/Odd Occurrences/Program.cs
+++ Programming Fundamentals C#/6.Dictionaries/Odd Occurrences/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Write a program that extracts from a given sequence of words all elements that present in it odd number of times (case-insensitive).
+•	Words are given in a single line, space separated.
+•	Print the result elements in lowercase, in their order of appearance.*/
+
 namespace OddOccurrences
 {
     class Program
