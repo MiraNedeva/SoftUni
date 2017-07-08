5c7e86cbf01193808a31e9407e40090918b180bd
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 21:11:07 +0300

    Upload of new homework exercices

--- Programming Fundamentals C#/5.Array And List Algorithms-Exercices/Shoot List Elements/Program.cs
+++ Programming Fundamentals C#/5.Array And List Algorithms-Exercices/Shoot List Elements/Program.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Until you receive the command "stop", you will either receive an integer, or the command bang on every line.
+If you receive an integer, place it at the front of the list. If, however, you receive the command bang, remove the first element whose 
+value is smaller than the average of the elements of the list, print "shot {element}" and then decrement every element in the list by 1.
+If you receive "bang" and there are no elements left in the list, print "nobody left to shoot! last one was {lastRemovedInt}" and end 
+the program. When you finally do receive the command "end", either print "survivors: {elements, separated by space}" if there are any, 
+or "you shot them all. last one standing was {lastRemovedInt}".*/
+
 namespace ShootListElements
 {
     class Program
