b1c288192d600226fd131b833695a8074d50ee35
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 22:32:00 +0300

    Upload of new homework exercices

--- Programming Fundamentals C#/4.Lists-Exercices/Ununion Lists/Program.cs
+++ Programming Fundamentals C#/4.Lists-Exercices/Ununion Lists/Program.cs
@@ -4,6 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* You will be given a sequence of integers, separated by a space. This is the primal list. Then you will receive an integer N. 
+On the next N lines, you will receive sequences of integers. Your task is to add all elements that the primal list does not contain from
+the current sequence to the primal list and then remove from the primal list, all elements which are contained in the current sequence of
+integers. If there are several occurrences, remove all occurrences you find.
+After you are done receiving lists and manipulating the primal list, sort the primal list and print it.
+Exemple: Input	              Output
+     1 2 3 4 5                1 14 16 20
+     3
+     3 4 5 20 1
+     16 2 1
+     14	*/
+
 namespace UnunionLists
 {
     class Program
