1d6968ca270ea07b49d0fbb7f3250ac7e340bcc9
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 20:04:30 +0300

    Upload of new homework exercices

--- Programming Fundamentals C#/8.Lambda And LINQ/Cottage Scraper/Program.cs
+++ Programming Fundamentals C#/8.Lambda And LINQ/Cottage Scraper/Program.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* You’re a carpenter at the local woodworking shop. Your client wants you to build a wooden skyscraper/cottage. A CottageScraper, he calls it. But in order to accomplish this task, he needs the trees to be taller than a certain height. The problem is that he doesn’t know what type of tree he’ll use yet, and doesn’t know how tall he wants to make the CottageScraper yet. You’ve obviously got nothing better to do, so you go to work chopping down logs until he calls you with the details.
+Write a program which receives tree types and their lengths in the format “{type} -> {height}”. When you receive the command “chop chop”, it’s time to get paid.
+On the next line, you will receive the type of tree that needs to be used to build the CottageScraper. On the final input line, you will receive the minimum length per tree, needed to accomplish the task. Filter the trees based on type and minimum length, making sure that you’ll only use the trees of the specified type and minimum length.
+After which, calculate the total price of the CottageScraper, which includes the price of all the trees you collected up to that point. The price is calculated as being the average meters of all logs you collected, per meter of log, rounded to the second decimal place.
+You’re going to charge the client 100% of the price per log for logs you’ll use in the skyscraper, and 25% of the price per log for logs you won’t use for the CottageScraper. Both of the fees are rounded to the second decimal place.
+After you make the calculations, print them on the console. On the first line of the console, print “Price per meter: ${pricePerMeter:F2}”. On the second, print “Used logs price: ${usedLogsPrice:F2}”. On the third line, print “Unused logs price: ${unusedLogsPrice:F2}”. 
+On the final line of the output, print “CottageScraper subtotal: ${subTotal:F2}”. */
+
 namespace CottageScraper
 {
     class Program
