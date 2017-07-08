49252dd3a5f1a2e89411f3d337c22a8d4b6e8ce0
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 20:50:13 +0300

    Upload of new homework exercices

--- Programming Fundamentals C#/6.Dictionaries/User Logins/Program.cs
+++ Programming Fundamentals C#/6.Dictionaries/User Logins/Program.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Write a program that receives a list of username-password pairs in the format “{username} -> {password}”. 
+If there’s already a user with that username, replace their password. After you receive the command “login”,login requests start coming in, using the same format. Your task is to print the status of user login, using different messages as per the conditions below:
+•	If the password matches with the user’s password, print “{username}: logged in successfully”.
+•	If the user doesn’t exist, or the password doesn’t match the user, print “{username}: login failed”. 
+When you receive the command “end”, print the count of unsuccessful login attempts, using the format “unsuccessful login attempts:{count}”.*/
+
 namespace UserLogins
 {
     class Program
