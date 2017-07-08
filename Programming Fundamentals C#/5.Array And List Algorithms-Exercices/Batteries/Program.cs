4cfbe72dda9abc08adf7111da230d42b81f1f2e7
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 21:58:45 +0300

    Upload of new homework exercices

--- Programming Fundamentals C#/5.Array And List Algorithms-Exercices/Batteries/Program.cs
+++ Programming Fundamentals C#/5.Array And List Algorithms-Exercices/Batteries/Program.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* You are in a battery manufacturing plant. Your task is to stress test the latest batch of batteries for longevity.
+You will receive an array of doubles on the console (first line, space-separated), indicating the capacities of the different batteries 
+in the batch (in mAh). On the next line, you will receive the usage per hour for each battery as an array of doubles (second line, space-separated).
+Next, you will receive the amount of hours you have to stress test each battery for (as an integer). Each of the batteries drains by its
+capacity until either the testing hours are over, or the battery dies (reaches 0 capacity).
+Print the status of all the batteries at the end of the testing session (in percentage). If any batteries die, along with the percentage, 
+print how many hours it took. The capacity and percentage are rounded to the 2nd decimal point.*/
+
 namespace Batteries
 {
     class Program
