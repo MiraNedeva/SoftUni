877a38e3daf77357f65f36aeddd97df64c7235f3
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 20:54:04 +0300

    Update Program.cs

--- Programming Fundamentals C#/6.Dictionaries/Fix Emails/Program.cs
+++ Programming Fundamentals C#/6.Dictionaries/Fix Emails/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* You are given a sequence of strings, each on a new line, unitll you receive “stop” command. First string is a name of a person. On the second line you receive his email. Your task is to collect their names and emails, and remove emails whose domain ends with "us" or "uk" (case insensitive). Print:
+{name} – > {email} */ 
+
 namespace FixEmails
 {
     class Program
@@ -14,25 +17,7 @@ namespace FixEmails
 
             string commandLine = Console.ReadLine();
 
-            /* while (! commandLine.Equals ("stop"))
-             {
-                 string email = Console.ReadLine();
-                 string ending = email.Substring(email.Length -2).ToLower(); 
-
-                 if (! ending.Equals("us") && ! ending.Equals("uk"))
-                 {
-                     emailsAdresses.Add(commandLine, email);
-                 }
-
-                 commandLine = Console.ReadLine();
-
-             }
-             foreach (var item in emailsAdresses)
-             {
-                 Console.WriteLine("{0} -> {1}",item.Key, item.Value);
-             }*/
-
-            while (!commandLine.Equals ("stop"))
+             while (!commandLine.Equals ("stop"))
             {
 
                 string email = Console.ReadLine();
