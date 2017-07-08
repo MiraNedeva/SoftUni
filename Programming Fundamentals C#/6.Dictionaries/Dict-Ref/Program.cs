e20b28cc264d96a006ae10ef79835d47282d84f4
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 20:44:39 +0300

    Upload of new homework exercices

--- Programming Fundamentals C#/6.Dictionaries/Dict-Ref/Program.cs
+++ Programming Fundamentals C#/6.Dictionaries/Dict-Ref/Program.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* You have been tasked to create a referenced dictionary, or in other words a dictionary, which knows how to reference itself.
+You will be given several input lines, in one of the following formats:
+•	{name} = {value}
+•	{name} = {secondName}
+The names will always be strings, and the values will always be integers.
+In case you are given a name and a value, you must store the given name and its value. If the name already EXISTS, you must CHANGE its value with the given one.
+In case you are given a name and a second name, you must store the given name with the same value as the value of the second name. If the given second name DOES NOT exist, you must IGNORE that input.
+When you receive the command “end”, you must print all entries with their value, by order of input, in the following format:
+{entry} === {value} */
+
 namespace Dict_Ref
 {
     class Program
