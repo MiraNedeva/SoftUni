d143433c3a6068b2424b82c35a9debe385440125
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 22:17:50 +0300

    Update Program.cs

--- Programming Fundamentals C#/4.Lists-Exercices/Equal Sum After Extraction/Program.cs
+++ Programming Fundamentals C#/4.Lists-Exercices/Equal Sum After Extraction/Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* You will be given two integer lists on the first two lines (space-separated). Remove the elements in the first list from the elements 
+in the second list. If an element from the first list occurs more than once in the second list, remove all occurrences.
+After you remove the elements, check the sum of both lists. If the sum of both of them is equal, print “Yes. Sum: {sum}”. 
+If not print the absolute difference between the two lists in the format “No. Diff: {sum}”.*/
+
 namespace EqualSumAfterExtraction
 {
     class Program
