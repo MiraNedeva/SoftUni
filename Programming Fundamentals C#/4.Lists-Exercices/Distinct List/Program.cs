e52fc20803341b6465d138fae35cf62285d0232a
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 22:24:41 +0300

    Update Program.cs

--- Programming Fundamentals C#/4.Lists-Exercices/Distinct List/Program.cs
+++ Programming Fundamentals C#/4.Lists-Exercices/Distinct List/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+  
+/* You will be given a list of integers on the first line of the input (space-separated). Remove all repeating elements from the list.*/
 
 namespace DistinctList
 {
