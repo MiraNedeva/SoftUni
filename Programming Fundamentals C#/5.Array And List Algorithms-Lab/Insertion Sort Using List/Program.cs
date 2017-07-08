72afea244b14af4de591090144a5dfbca3ad1270
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 21:07:26 +0300

    Upload of new homework exercices

--- Programming Fundamentals C#/5.Array And List Algorithms-Lab/Insertion Sort Using List/Program.cs
+++ Programming Fundamentals C#/5.Array And List Algorithms-Lab/Insertion Sort Using List/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Read a list of integers on the first line of the console.After that, sort the array, using the Insertion Sort algorithm, 
+but instead of doing it in-place, add the result one by one to a list. */
+
 namespace InsertionSortUsingList
 {
     class Program
