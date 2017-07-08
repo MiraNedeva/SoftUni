95adb01ea8b9f681245d0c2cca772132f6a93c01
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 20:38:02 +0300

    Update Program.cs

--- Programming Fundamentals C#/7.Advanced Collections/Dict-Ref Advanced/Program.cs
+++ Programming Fundamentals C#/7.Advanced Collections/Dict-Ref Advanced/Program.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Remember the DIct-Ref Problem from the previous exercise? Well this one is an Advanced Version.
+You will begin receiving input lines containing information in one of the following formats:
+•	{key} -> {value1, value2, value3. . .}
+•	{key} -> {otherKey}
+The keys will always be strings, and the values will always be integers, separated by a comma and a space. 
+If you are given a key and values, you must store the values to the given key. If the key already exists, you must add the given values to the old ones.
+If you are given a key and another key, you must copy the values of the other key to the first one. If the other key does not exist, this input line must be IGNORED.
+When you receive the command “end”, you must stop reading input lines, and you must print all keys with their values, in the following format:
+{key} === {value1, value2, value3. . .} */
+
 namespace DictRefAdvanced
 {
     class Program
