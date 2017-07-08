470a93fff3d44ad7cd5947cd7d66cec52de9a827
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 22:08:02 +0300

    Update Program.cs

--- Programming Fundamentals C#/4.Lists-Lab/Append Lists/Program.cs
+++ Programming Fundamentals C#/4.Lists-Lab/Append Lists/Program.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Write a program to append several lists of numbers.
+	Lists are separated by ‘|’.
+	Values are separated by spaces (‘ ’, one or several)
+	Order the lists from the last to the first, and their values from left to right.
+*/
+
 namespace AppendLists
 {
     class Program
