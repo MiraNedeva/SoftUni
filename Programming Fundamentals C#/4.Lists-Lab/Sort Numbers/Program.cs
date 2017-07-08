e6b3431be9f880ce2966db685dfb57cd4b372a7d
MiraNedeva <mira.nedeva@yahoo.fr>
2017-07-08 22:12:23 +0300

    Update Program.cs

--- Programming Fundamentals C#/4.Lists-Lab/Sort Numbers/Program.cs
+++ Programming Fundamentals C#/4.Lists-Lab/Sort Numbers/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+ 
+/* Read a list of decimal numbers and sort them in increasing order. Print the output as shown in the examples below.
+Input	          Output
+8 2 7 3	       2 <= 3 <= 7 <= 8
+2 4 -9	      -9 <= 2 <= 4         */
 
 namespace SortNumbers
 {
